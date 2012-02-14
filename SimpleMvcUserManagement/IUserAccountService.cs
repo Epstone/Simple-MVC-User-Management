ef3eb30af5c31f25bd1a461b131a0ff5d2cedca7
Patrick <Patrickeps@gmx.de>
2012-02-14 15:18:50 +0100

    - added dummy memberhip and role provider for demo page - some changes to the ui styling

---
+++ SimpleMvcUserManagement/IUserAccountService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+
+namespace SimpleMvcUserManagement
+{
+  public interface IUserAccountService
+  {
+    MembershipUserCollection GetAllUsers(int page, int size, out int totalRecords);
+    MembershipUserCollection GetAllUsers();
+
+    void DeleteUser(object userId);
+    MembershipUser CreateUser(string username, string password, string email, out MembershipCreateStatus createStatus);
+
+    string[] GetAllRoles();
+
+    void AddUserToRoles(MembershipUser user, string[] roles);
+
+    void CreateRole(string roleName);
+
+    bool DeleteRole(string roleName, bool throwOnPopulatedRole);
+  }
+}
